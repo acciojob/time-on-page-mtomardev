2f3ab33e9a3026b9ab7201fae84b8a8a0a330fb9
mtomardev <120013813+mtomardev@users.noreply.github.com>
2024-05-13 09:31:37 +0530

    second

--- src/components/App.js
+++ src/components/App.js
@@ -18,8 +18,8 @@ const App = () => {
   return (
     <div>
       {/* Do not remove the main div */}
-      <p>You\'ve been on this page for 0 seconds.</p>
-      <p>You\'ve been on this page for {seconds} seconds.</p>
+      {/* <p>You\'ve been on this page for 0 seconds.</p> */}
+      <p>You've been on this page for {seconds} seconds.</p>
     </div>
   );
 }
